0ca6eadb76810c602d68c692204cc5c5bba6166a
Shashi Yadav <yadavshashi366@gmail.com>
2021-11-22 00:25:36 +0530

    Initial

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "",
+    link: "https://linktr.ee/shashi0896",
+  },
+  {
+    id: 2,
+    img: "",
+    link: "https://linktr.ee/shashi0896",
+  },
+  {
+    id: 3,
+    img: "",
+    link: "https://linktr.ee/shashi0896",
+  },
+  {
+    id: 4,
+    img: "",
+    link: "https://linktr.ee/shashi0896",
+  },
+  {
+    id: 5,
+    img: "",
+    link: "https://linktr.ee/shashi0896",
+  },
+  {
+    id: 6,
+    img: "",
+    link: "https://linktr.ee/shashi0896",
+  },
+];
